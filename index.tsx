c12573267bc2c94d8c5cd9269a50874f8a39f0f8
sids4ru <sids4ru@gmail.com>
2023-01-31 06:37:08 +0530

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,16 @@
+import { SandboxApp } from "@fluentui/code-sandbox"
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import Example from "./example";
+
+//
+// You can edit this example in "example.js" or "example.tsx".
+//
+
+ReactDOM.render(
+  <SandboxApp>
+    <Example />
+  </SandboxApp>,
+  document.getElementById("root")
+);
